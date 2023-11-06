84547dc73438d6dbc98efb1d36eaa9b398d12b3e
Maahira <maahiraislam.2001@gmail.com>
2023-11-06 19:48:23 +0000

    Creates functions to organise code better

--- milestone_3.py
+++ milestone_3.py
@@ -3,14 +3,23 @@ import random
 word_list = ["Strawberries","Raspberries","Apples","Oranges","Mangoes"]
 word = random.choice(word_list)
 
-while True:
-    guess = input("Guess a letter: ")
-    if guess.isalpha() and len(guess) == 1:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.") 
+
+def ask_for_input():
+    guess = input("Guess a letter: ")
+    while True:
+        if guess.isalpha() and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+            guess = input("Guess a letter: ")
+    check(guess)
+
+def check(guess):
+    guesses = guess.lower()
+    if guesses in word:
+        print(f"Good guess! {guesses} is in the word.")
+    else:
+        print(f"Sorry, {guesses} is not in the word. Try again.") 
+
+ask_for_input()
